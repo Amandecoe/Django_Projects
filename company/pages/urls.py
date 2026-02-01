bbf219aac6616ab024598ed4547c1d3cd5d35665
Amanuel <amanuelbehailu557@gmail.com>
2026-02-01 12:41:29 +0300

    template views

---
+++ company/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home_page_view, AboutPageView
+
+urlpatterns = [
+    path("", home_page_view),
+    path("about/", AboutPageView.as_view()) #as.view is added because AboutPageView is class based view
+                                            # as.view makes the class based view callable, it is mandatory to add it when using class based views
+]
--- company/pages/views.py
+++ company/pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Jordan", "Nike", "Addidas"],
+        "greeting":"Thank You For Visiting.",
+    }
+    return render(request, "home.html", context)
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
