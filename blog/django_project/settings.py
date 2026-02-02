d46c32b062df4ee00087497dbb35247abf81e4b1
Amanuel <amanuelbehailu557@gmail.com>
2026-02-02 13:32:33 +0300

    static files

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] #defines the additional location the built-in staticfiles app will traverse looking for static files beyond an app/static folder
