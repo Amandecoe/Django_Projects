bf6ad176abdc2c2beb8f94515cffc2e29f1506fe
Amanuel <amanuelbehailu557@gmail.com>
2026-02-02 13:07:37 +0300

    models defined

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.user",
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+
