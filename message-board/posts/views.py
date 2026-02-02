f5381f8a508cd768ae1c9e72e13fc0dcd698eb9c
Amanuel <amanuelbehailu557@gmail.com>
2026-02-02 10:39:03 +0300

    db data displayed

--- message-board/posts/views.py
+++ message-board/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all() #database query to get all objects of the model, objects is a manager which gives access to the db
+    return render (request, "post_list.html", {"posts": posts}) #the context dictionary here displays our posts from the db in a "post": hello world format
+
