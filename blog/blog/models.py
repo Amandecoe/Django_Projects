9f6c8a1cf81d07fd3fc3f4f356dde0f0a4c109d0
Amanuel <amanuelbehailu557@gmail.com>
2026-02-10 08:45:58 +0300

    reverse keyword used for the url post_detail

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -12,6 +13,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs = {"pk":self.pk})
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         "title",
