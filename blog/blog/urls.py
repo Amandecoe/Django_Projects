3e9fb4a3a64f9ca048e475f9c0f3fb3b7e680750
Amanuel <amanuelbehailu557@gmail.com>
2026-02-10 11:02:05 +0300

    ListView and DetailView implemented

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import BlogDetailView, BlogListView
 
 urlpatterns = [
-    path("", post_list, name = "home"),
-    path("post/<int:pk>/", post_detail, name = "post_detail"),
+    path("", BlogListView.as_view(), name = "home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name = "post_detail"),
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "home.html", {'posts': posts})
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = 'posts'
 
-def post_detail(request, pk): #pk = the primary key of a blog post
-    post = get_object_or_404(Post, pk=pk)
-    return render (request, "post_detail.html", {"post":post})
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
