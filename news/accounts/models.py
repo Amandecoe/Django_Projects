8d7bd9aeb7c702400085d81d6d4c27cf8db2fed5
Amanuel <amanuelbehailu557@gmail.com>
2026-02-22 17:31:25 +0300

    starting

--- news/accounts/models.py
+++ news/accounts/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    age = models.PositiveBigIntegerField(null = True, blank = True)
+    age = models.PositiveBigIntegerField(null = True, blank = True) 
