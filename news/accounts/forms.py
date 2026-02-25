f50c5082b0fabf615ba94cd46757691e7406c5ec
Amanuel <amanuelbehailu557@gmail.com>
2026-02-25 11:18:57 +0300

    user creation form fields edited i.e emails added

--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -4,9 +4,17 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",) #Meta.fields helps us use the default fields defined in the user model
+        fields = (
+            'username',
+            'email',
+            'age',
+        )
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            'username',
+            'email',
+            'age',
+        )
