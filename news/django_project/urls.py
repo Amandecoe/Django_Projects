24a745fb8e69022a6f292b27b8a5a131a27f942b
Amanuel <amanuelbehailu557@gmail.com>
2026-02-25 15:16:26 +0300

    articles views

--- news/django_project/urls.py
+++ news/django_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")), #the built in auth app provides views and URLs for logging in and out
     path("", TemplateView.as_view(template_name = "home.html"), name = "home"), #uses the template_name shortcut to show the home template
+    path("articles/", include("articles.urls")),
     path("", include("pages.urls")),
 ]
