57490572a03d4b09a6c3c109078865d471cb4b40
Amanuel <amanuelbehailu557@gmail.com>
2026-02-26 10:41:41 +0300

    new and delete not working

--- news/articles/views.py
+++ news/articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.views.generic import ListView, DetailView
 from django.urls import reverse_lazy
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 # Create your views here.
 
 class ArticleListView(ListView):
@@ -26,3 +26,12 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_engine = "article_delete.html"
     success_url = reverse_lazy("article_list") #where you are redirected after deleting an article
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        "title",
+        "body",
+        "author",
+    )
